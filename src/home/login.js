f5ba726757df5771196b18ffbeae078adc761aaa
rohan <r0han45dev@gmail.com>
2023-09-21 15:30:48 +0530

    chnage

--- src/home/login.js
+++ src/home/login.js
@@ -3,6 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 function Login() {
     const navigate = useNavigate()
+    const ListofArray = [{ 1: "da" }, { 2: "fa" }, { 4: "da" }]
+    console.log(ListofArray )
+    const getUpperCase = ListofArray.reduce((previousvalue, currentvalue, currentindex) => {
+        console.log({ ...previousvalue, ...currentvalue })
+        return { ...previousvalue, ...currentvalue }
+    })
+
+    // console.log(getUpperCase)
     return (
         <>
             <div>Login</div>
